bfeb63978b29de03951224356c904ec0b09fd909
주민재 <73321655+Sharon77770@users.noreply.github.com>
2022-10-24 12:30:14 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot: 
         return
-    if message.content == '':
+    if message.content == '!help':
         await message.channel.send('.')
 
 
