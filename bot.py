77bbecc70fa9a757aadc53442105158a77f98664
주민재 <73321655+Sharon77770@users.noreply.github.com>
2022-10-24 12:46:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.utils import get
 from urllib import request
 from bs4 import BeautifulSoup
 
-intents=discord.Intents.default()
+intents=discord.Intents.all()
 bot = Bot(command_prefix='!', intents=intents, help_command=None)
 userList = {}
 
